2a1daa7569a9e381a2aa75f4079c027a30195997
Juan Cruz Perez Porcel <perezporcel.juancruz@gmail.com>
2023-01-16 23:55:56 -0300

    v1.0.1

--- Assets/Scripts/Weapon/Necronomicon.cs
+++ Assets/Scripts/Weapon/Necronomicon.cs
@@ -124,6 +124,11 @@ public class Necronomicon : Weapon
                 weaponData.WeaponImage = evSprite;
                 weaponData.ItemName = evolutionName;
                 break;
+
+            default:
+                weaponData.ItemName = normalName;
+                weaponData.WeaponImage = normalSprite;
+                break;
         }
     }
 }
